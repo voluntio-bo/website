b9488768e7c137ff0028c6307da5e1d1586f98cb
Kevin <kevcespedesarancibia87@gmail.com>
2023-04-29 11:56:22 -0400

    themes included

--- src/index.js
+++ src/index.js
@@ -6,15 +6,46 @@ import Reaction from './components/Reaction';
 import NavBar from './components/NavBar';
 import EventCard from './components/EventCard';
 import reportWebVitals from './reportWebVitals';
+import { createTheme, ThemeProvider, styled, responsiveFontSizes } from '@mui/material/styles'
+  
+let theme = createTheme({
+typography: {
+    fontFamily: `"Sora", sans-serif`,
+    fontWeightRegular: 500,
+    fontSize: 16,
+    // h1 : {
+    //     fontFamily : `"Poppins", sans-serif`,
+    // },
+    // h2 : {
+    //     fontFamily : `"DM Sans", sans-serif`,
+    //     fontWeight : 600,
+    //     fontSize : 35
+    // },
+    // h3 : {
+    //     fontFamily : `"DM Sans", sans-serif`,
+    //     fontWeight : 600,
+    //     fontSize : 30,
+    // },
+    // h4 : {
+    //     fontFamily : `"DM Sans", sans-serif`,
+    //     fontWeight : 600,
+    //     fontSize : 25
+    // },
+    h6 : {
+        fontWeight : 500,
+        fontSize : 20,
+        //letterSpacing: 0,
+    },
+    subtitle2 : {
+        fontFamily : `"DM Sans", sans-serif`,
+        fontWeight : 500,
+        fontSize : 14,
+        color : '#989a9f',
+    }
+},
+});
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <EventCard />
-  </React.StrictMode>
-);
+theme = responsiveFontSizes(theme);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<ThemeProvider theme={theme}><App/></ThemeProvider>)
