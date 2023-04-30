202ac26c849d1e3368d1a69c41cd61074ce4a3bb
Kevin <kevcespedesarancibia87@gmail.com>
2023-04-29 20:06:07 -0400

    Using imagePath and eventDate

--- src/views/MainWall.js
+++ src/views/MainWall.js
@@ -19,11 +19,7 @@ function MainWall(){
     useEffect(() => { 
         fetchEventsData();
     }, [])
-
-    // let events = [
-    //     {"name":"Nombre 1","description":"Description"},
-    //     {"name":"Nombre 2","description":"Description"}
-    // ]
+    
     return (
         <Box sx={{display:'flex', flexDirection:'column', alignItems:'center'}}>
             <Box sx={{width:{sm:0.9,xs:0.9,md:0.55}}}>
