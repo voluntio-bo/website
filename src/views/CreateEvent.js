85518961adbbfdbe839dc18c31875dc18662a0df
CaritoHidalgo <carolina.hidalgo@assuresoftbolivia.com>
2023-04-30 01:07:58 -0400

    createv1

--- src/views/CreateEvent.js
+++ src/views/CreateEvent.js
@@ -7,41 +7,67 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import BasicButtons from ".././components/Button";
 function CreateEvent() {
-  return (
-    <Box sx={{justifyContent: 'center',paddingTop:4,}}>
-    <Typography variant="h6" sx={{marginTop: '7px', textAlign: 'center'}}>
-        <h1>Crear Evento</h1>
-    </Typography>
-    <Typography variant="h7" sx={{marginTop: '7px', justifyContent: 'rigth'}}>
-     <h2>Tipo de Evento</h2>
-     <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center',justifyContent: 'center'}}>
-  <MyChip label="Interacción Humana" src="https://img.europapress.es/fotoweb/fotonoticia_20150331134913-15031252339_400.jpg" />
-  <MyChip label="Construcción" src="https://img.europapress.es/fotoweb/fotonoticia_20150331134913-15031252339_400.jpg" />
-  <MyChip label="Interacción Humana" src="https://img.europapress.es/fotoweb/fotonoticia_20150331134913-15031252339_400.jpg" />
-  <MyChip label="Cuidado animal" src="https://img.europapress.es/fotoweb/fotonoticia_20150331134913-15031252339_400.jpg" />
-  <MyChip label="Ambiental" src="https://img.europapress.es/fotoweb/fotonoticia_20150331134913-15031252339_400.jpg" />
-  <MyChip label="Recaudación de fondos" src="https://img.europapress.es/fotoweb/fotonoticia_20150331134913-15031252339_400.jpg" />
-  <MyChip label="Salud" src="https://img.europapress.es/fotoweb/fotonoticia_20150331134913-15031252339_400.jpg" />
-  <MyChip label="Recaudación de viveres" src="https://img.europapress.es/fotoweb/fotonoticia_20150331134913-15031252339_400.jpg" />
-  <MyChip label="Otros" src="https://img.europapress.es/fotoweb/fotonoticia_20150331134913-15031252339_400.jpg" />
-</Box>
-
-     <h2>Descripcion breve</h2>
-     <BasicTextFields></BasicTextFields>
-     <h2>Nombre del Evento</h2>
-     <BasicTextFields></BasicTextFields>
-     <h2>Fecha</h2>
-     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DatePicker />
-    </LocalizationProvider>
-     <h2>Duración</h2>
-     <BasicTextFields></BasicTextFields>
-     <Box sx={{ display: 'flex', justifyContent: 'center', paddingTop: 4, paddingBottom: 4}}>
-    <BasicButtons color="green" text="Enviar" />
-  </Box>
-    </Typography>
-    </Box>
-  );
+    return (
+        <Box
+            sx={{
+                position: 'absolute',
+                top: '70%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '110vh',
+                width: '80vw',
+                maxWidth: '700px',
+                borderRadius: 16,
+                boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.25)',
+                padding: 4,
+                border: '3px solid orange',
+            }}
+        >
+            <Box sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '100%',
+                maxWidth: '600px',
+                
+            }}>
+                <Typography variant="h4" sx={{ marginTop: '7px', textAlign: 'center', fontWeight: 'bold', color: 'orange' }}>
+                    Crear Evento
+                </Typography>
+                <Typography variant="h5" sx={{ marginTop: '7px', justifyContent: 'right', color: 'orange', marginBottom: '7px' }}>
+                    Tipo de Evento
+                    <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'center', padding: 1, width: '100%' }}>
+                        <MyChip label="Interacción Humana" src="https://www.menudoscorazones.org/wp-content/uploads/2018/09/Como-funciona-el-corazon.png" />
+                        <MyChip label="Construcción" src="https://www.menudoscorazones.org/wp-content/uploads/2018/09/Como-funciona-el-corazon.png" />
+                        <MyChip label="Cuidado animal" src="https://www.menudoscorazones.org/wp-content/uploads/2018/09/Como-funciona-el-corazon.png" />
+                        <MyChip label="Ambiental" src="https://www.menudoscorazones.org/wp-content/uploads/2018/09/Como-funciona-el-corazon.png" />
+                        <MyChip label="Recaudación de fondos" src="https://www.menudoscorazones.org/wp-content/uploads/2018/09/Como-funciona-el-corazon.png" />
+                        <MyChip label="Salud" src="https://www.menudoscorazones.org/wp-content/uploads/2018/09/Como-funciona-el-corazon.png" />
+                        <MyChip label="Recaudación de viveres" src="https://www.menudoscorazones.org/wp-content/uploads/2018/09/Como-funciona-el-corazon.png" />
+                        <MyChip label="Otros" src="https://www.menudoscorazones.org/wp-content/uploads/2018/09/Como-funciona-el-corazon.png" />
+                    </Box><br></br>
+                    Descripcion breve
+                    <BasicTextFields></BasicTextFields>
+                    Nombre del Evento
+                    <BasicTextFields></BasicTextFields>
+                    Fecha
+                    <br></br><LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DatePicker /><br></br>
+                    </LocalizationProvider><br></br>
+                    Duración
+                    <br></br><BasicTextFields></BasicTextFields>
+                    <Box sx={{ display: 'flex', justifyContent: 'center', paddingTop: 4 }}>
+                        <BasicButtons color="green" text="Enviar" />
+                    </Box>
+                </Typography>
+            </Box>
+        </Box>
+    );
 }
 
 export default CreateEvent;
